d3623528960ca0eaecc7684cd7ef6412616117d3
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-10 08:24:36 +0700

    feat: Artcles & Projects Card Components

---
+++ components/card/Articles.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card-articles">
+    <div class="card-articles__img">
+      <img src="@/assets/img/static/home/home-article-1.jpg" alt="Projects 1" />
+      <div class="card-articles__img--tag">
+        <p>{{ item.tag }}</p>
+      </div>
+    </div>
+    <h3 class="card-articles__title">
+      {{ item.title }}
+    </h3>
+    <div class="card-articles__content">
+      <p class="card-articles__content--date">
+        {{ item.date }}
+      </p>
+      <div class="card-articles__content--button">
+        <img src="@/assets/img/icon/chevron-right.svg" alt="Icon Chevron" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardArticles',
+  props: {
+    item: Object,
+  },
+}
+</script>
---
+++ components/card/Projects.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card-projects">
+    <img src="@/assets/img/static/home/home-project-1.jpg" alt="Projects 1" />
+    <div class="card-projects__content">
+      <div class="card-projects__content--left">
+        <h3 class="card-projects__content--left__title">
+          {{ item.title }}
+        </h3>
+        <p class="card-projects__content--left__subtitle">
+          {{ item.desc }}
+        </p>
+      </div>
+      <div class="card-projects__content--right">
+        <img src="@/assets/img/icon/chevron-right.svg" alt="Icon Chevron" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardProjects',
+  props: {
+    item: Object,
+  },
+}
+</script>
