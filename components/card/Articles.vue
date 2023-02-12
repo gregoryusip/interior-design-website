4321acb2d39cfb1aa1b39a0eb3deaa18717a0961
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-12 13:42:16 +0700

    fix: card articles use nuxt-link

--- components/card/Articles.vue
+++ components/card/Articles.vue
@@ -1,23 +1,28 @@
 <template>
-  <div class="card-articles">
-    <div class="card-articles__img">
-      <img src="@/assets/img/static/home/home-article-1.jpg" alt="Projects 1" />
-      <div class="card-articles__img--tag">
-        <p>{{ item.tag }}</p>
+  <nuxt-link :to="`/blogs/${item.id}`">
+    <div class="card-articles">
+      <div class="card-articles__img">
+        <img
+          :src="require(`@/assets/img/static/blogs/${item.img}`)"
+          alt="Projects"
+        />
+        <div class="card-articles__img--tag">
+          <p>{{ item.tag }}</p>
+        </div>
+      </div>
+      <h3 class="card-articles__title">
+        {{ item.title }}
+      </h3>
+      <div class="card-articles__content">
+        <p class="card-articles__content--date text--subtitle">
+          {{ item.date }}
+        </p>
+        <div class="card-articles__content--button">
+          <img src="@/assets/img/icon/chevron-right.svg" alt="Icon Chevron" />
+        </div>
       </div>
     </div>
-    <h3 class="card-articles__title">
-      {{ item.title }}
-    </h3>
-    <div class="card-articles__content">
-      <p class="card-articles__content--date">
-        {{ item.date }}
-      </p>
-      <div class="card-articles__content--button">
-        <img src="@/assets/img/icon/chevron-right.svg" alt="Icon Chevron" />
-      </div>
-    </div>
-  </div>
+  </nuxt-link>
 </template>
 
 <script>
