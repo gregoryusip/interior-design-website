43a2deeed99fb6a8207d3a477b869f65768b53cc
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-12 13:43:11 +0700

    feat: new PageHeader UI Component

---
+++ components/ui/PageHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page-header">
+    <img
+      :src="require(`@/assets/img/header/${img}`)"
+      alt="Header Image"
+      class="page-header--img"
+    />
+    <div v-if="titleHeader" class="page-header--wrapper">
+      <h1 class="page-header--wrapper__title">{{ title }}</h1>
+      <p class="page-header--wrapper__subtitle text--subtitle">
+        {{ subtitle }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  props: {
+    img: {
+      type: String,
+      default: 'services.jpg',
+    },
+    title: {
+      type: String,
+      default: 'Services',
+    },
+    subtitle: {
+      type: String,
+      default: 'Home / Services',
+    },
+    titleHeader: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
