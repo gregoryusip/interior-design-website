fea2fc3ec2a0f26b06c093c3a7e357654a93c496
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-12 13:42:29 +0700

    feat: New Card Latest Articles Component

---
+++ components/card/LatestArticles.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-latest-articles">
+    <h6 class="card-latest-articles--title">{{ title }}</h6>
+    <p class="card-latest-articles--date">{{ date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardLatestArticles',
+  props: {
+    title: {
+      type: String,
+      default: 'We Focus On Comfort And Gorgeous',
+    },
+    date: {
+      type: String,
+      default: '06/02/2020',
+    },
+  },
+}
+</script>
