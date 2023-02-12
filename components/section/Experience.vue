4308526be9c06055e72606c74f2c6adcc6152b75
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-12 13:42:42 +0700

    feat: new section Experience component

---
+++ components/section/Experience.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="section--experience">
+    <div class="site-container">
+      <div class="section--experience__container">
+        <div
+          v-for="(item, i) in points"
+          :key="`point-${i}`"
+          class="section--experience__container--wrapper"
+        >
+          <h1 class="section--experience__container--wrapper__title text--85">
+            {{ item.count }}
+          </h1>
+          <p class="section--experience__container--wrapper__subtitle">
+            {{ item.label }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionExperience',
+  props: {
+    points: Array,
+  },
+}
+</script>
