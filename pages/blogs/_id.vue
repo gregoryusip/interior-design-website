42e9427143ca6ab7e457acaad183630e3b012249
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-12 14:15:17 +0700

    fix: update layout blogs detail

--- pages/blogs/_id.vue
+++ pages/blogs/_id.vue
@@ -4,9 +4,9 @@
     <UiPageHeader :img="'banner-detail.jpg'" :titleHeader="false" />
 
     <!-- LATEST POST -->
-    <div class="site-container py-20 lg:py-36">
+    <div class="site-container py-10 lg:py-20">
       <div class="blogs--detail__container">
-        <div class="blogs--detail__container--left">
+        <div class="blogs--detail__container--left mb-10 lg:mb-0">
           <div class="blogs--detail__content">
             <h1 class="blogs--detail__content--title">
               Let’s Get Solution for Building Construction Work
@@ -32,7 +32,7 @@
                 necessary.
               </p>
             </div>
-            <div class="blogs--detail__content--quote mb-20">
+            <div class="blogs--detail__content--quote mb-10 lg:mb-20">
               <img
                 src="@/assets/img/icon/quote.svg"
                 alt="Quote Icon"
@@ -47,7 +47,7 @@
             <h1 class="blogs--detail__content--title">
               Design sprints are great
             </h1>
-            <div class="mt-5 mb-8">
+            <div class="mt-5 mb-5">
               <p class="text--subtitle">
                 Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
                 turpmaximus.posuere in.Contrary to popular belief.There are many
@@ -114,66 +114,6 @@
                 </a>
               </div>
             </div>
-
-            <div class="section--form-replay">
-              <h4 class="section--form-replay__title">Leave a Reply</h4>
-              <form action="">
-                <div class="section--form-replay__two mb-10">
-                  <div class="section--form-replay__two--wrapper">
-                    <input
-                      type="text"
-                      placeholder="Name"
-                      class="section--form-replay__form"
-                    />
-                  </div>
-                  <div class="section--form-replay__two--wrapper">
-                    <input
-                      type="email"
-                      placeholder="Email"
-                      class="section--form-replay__form"
-                    />
-                  </div>
-                </div>
-                <div class="section--form-replay__two mb-10">
-                  <div class="section--form-replay__two--wrapper">
-                    <input
-                      type="text"
-                      placeholder="Website"
-                      class="section--form-replay__form"
-                    />
-                  </div>
-                  <div class="section--form-replay__two--wrapper">
-                    <input
-                      type="text"
-                      placeholder="Phone"
-                      class="section--form-replay__form"
-                    />
-                  </div>
-                </div>
-                <div class="section--form-replay__one mb-4">
-                  <div class="section--form-replay__one--wrapper">
-                    <textarea
-                      name="textarea"
-                      id="textarea"
-                      placeholder="Hello I'm Interested in..."
-                      rows="3"
-                      class="section--form-replay__form"
-                    ></textarea>
-                  </div>
-                </div>
-                <div class="section--form-replay__one mb-10">
-                  <div class="section--form-replay__one--wrapper">
-                    <input type="checkbox" class="" />
-                    <label for="" class=""
-                      >Save my name, email, and website in this browser for the
-                      next time I comment.</label
-                    >
-                  </div>
-                </div>
-
-                <UiButtonArrow class="button__dark" :text="'Send Now'" />
-              </form>
-            </div>
           </div>
         </div>
 
@@ -222,6 +162,71 @@
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- FORM REPLY -->
+    <div class="site-container pb-20 lg:pb-36">
+      <div class="blogs--detail__form lg:px-8">
+        <div class="section--form-replay">
+          <h4 class="section--form-replay__title">Leave a Reply</h4>
+          <form action="">
+            <div class="section--form-replay__two mb-10">
+              <div class="section--form-replay__two--wrapper">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  class="section--form-replay__form"
+                />
+              </div>
+              <div class="section--form-replay__two--wrapper">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  class="section--form-replay__form"
+                />
+              </div>
+            </div>
+            <div class="section--form-replay__two mb-10">
+              <div class="section--form-replay__two--wrapper">
+                <input
+                  type="text"
+                  placeholder="Website"
+                  class="section--form-replay__form"
+                />
+              </div>
+              <div class="section--form-replay__two--wrapper">
+                <input
+                  type="text"
+                  placeholder="Phone"
+                  class="section--form-replay__form"
+                />
+              </div>
+            </div>
+            <div class="section--form-replay__one mb-4">
+              <div class="section--form-replay__one--wrapper">
+                <textarea
+                  name="textarea"
+                  id="textarea"
+                  placeholder="Hello I'm Interested in..."
+                  rows="3"
+                  class="section--form-replay__form"
+                ></textarea>
+              </div>
+            </div>
+            <div class="section--form-replay__one mb-10">
+              <div class="section--form-replay__one--wrapper">
+                <input type="checkbox" class="" />
+                <label for="" class=""
+                  >Save my name, email, and website in this browser for the next
+                  time I comment.</label
+                >
+              </div>
+            </div>
+
+            <UiButtonArrow class="button__dark" :text="'Send Now'" />
+          </form>
         </div>
       </div>
     </div>
