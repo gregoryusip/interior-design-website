d6b65d054ac57a4071a49820101b672277849955
Gregoryus Imannuel Perdana <gregoryus.imannuel200@gmail.com>
2023-02-12 13:42:53 +0700

    feat: new Join Us Section Component

---
+++ components/section/JoinUs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="site-container pb-20 lg:pb-36">
+    <div class="section--join">
+      <h1 class="section--join__title">Wanna join the interno?</h1>
+      <p class="section--join__subtitle">
+        It is a long established fact will be distracted.
+      </p>
+      <UiButtonArrow class="button__brown" :text="'Contact With Us'" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionJoinUs',
+}
+</script>
